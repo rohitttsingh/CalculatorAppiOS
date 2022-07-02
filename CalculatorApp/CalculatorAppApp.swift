bedb943fb4042ee0d17c651d5b5b36da2ca171be
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-07-03 00:37:06 +0530

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Rohit Singh on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
